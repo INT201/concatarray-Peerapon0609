b4d07a05aa6f7a6cd63ed0e3326fa5c95c481243
Peerapon <peerapomsri2545@gmail.com>
2022-09-11 13:20:29 +0700

    finish

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,6 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  return (array1==null||undefined)&&(array2==null||undefined)?undefined:(array1==null||undefined)?array2:(array2==null||undefined)?array1:array1.concat(array2)
 }
 module.exports = concatArray
